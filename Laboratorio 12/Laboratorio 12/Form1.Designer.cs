5584b09d3faee99e5ec08aa9abe14306efd04322
Josemanuel Sifontes <josemanuelsv18@gmail.com>
2025-10-19 20:41:07 -0500

    Laboratorio 11 y 12 en proceso

---
+++ Laboratorio 12/Laboratorio 12/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Laboratorio_12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblVelocidad = new System.Windows.Forms.Label();
+            this.lblTiempo = new System.Windows.Forms.Label();
+            this.txtVelocidad = new System.Windows.Forms.TextBox();
+            this.txtTiempo = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtRespuesta = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(109, 25);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(110, 13);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Calculadora Distancia";
+            // 
+            // lblVelocidad
+            // 
+            this.lblVelocidad.AutoSize = true;
+            this.lblVelocidad.Location = new System.Drawing.Point(18, 70);
+            this.lblVelocidad.Name = "lblVelocidad";
+            this.lblVelocidad.Size = new System.Drawing.Size(57, 13);
+            this.lblVelocidad.TabIndex = 1;
+            this.lblVelocidad.Text = "Velocidad:";
+            // 
+            // lblTiempo
+            // 
+            this.lblTiempo.AutoSize = true;
+            this.lblTiempo.Location = new System.Drawing.Point(28, 129);
+            this.lblTiempo.Name = "lblTiempo";
+            this.lblTiempo.Size = new System.Drawing.Size(45, 13);
+            this.lblTiempo.TabIndex = 2;
+            this.lblTiempo.Text = "Tiempo:";
+            // 
+            // txtVelocidad
+            // 
+            this.txtVelocidad.Location = new System.Drawing.Point(81, 67);
+            this.txtVelocidad.Name = "txtVelocidad";
+            this.txtVelocidad.Size = new System.Drawing.Size(187, 20);
+            this.txtVelocidad.TabIndex = 3;
+            // 
+            // txtTiempo
+            // 
+            this.txtTiempo.Location = new System.Drawing.Point(81, 126);
+            this.txtTiempo.Name = "txtTiempo";
+            this.txtTiempo.Size = new System.Drawing.Size(187, 20);
+            this.txtTiempo.TabIndex = 4;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(31, 188);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(112, 188);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 6;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(193, 188);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Respueta";
+            // 
+            // txtRespuesta
+            // 
+            this.txtRespuesta.Location = new System.Drawing.Point(81, 246);
+            this.txtRespuesta.Name = "txtRespuesta";
+            this.txtRespuesta.Size = new System.Drawing.Size(187, 20);
+            this.txtRespuesta.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 361);
+            this.Controls.Add(this.txtRespuesta);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtTiempo);
+            this.Controls.Add(this.txtVelocidad);
+            this.Controls.Add(this.lblTiempo);
+            this.Controls.Add(this.lblVelocidad);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblVelocidad;
+        private System.Windows.Forms.Label lblTiempo;
+        private System.Windows.Forms.TextBox txtVelocidad;
+        private System.Windows.Forms.TextBox txtTiempo;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtRespuesta;
+    }
+}
+
---
+++ Laboratorio 12/Laboratorio 12/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Laboratorio_12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            // Verificar que los campos no estén vacíos
+            if (string.IsNullOrWhiteSpace(txtVelocidad.Text) || string.IsNullOrWhiteSpace(txtTiempo.Text))
+            {
+                MessageBox.Show("Por favor, ingrese ambos valores: velocidad y tiempo.", "Datos incompletos",
+                              MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Validar que los valores sean numéricos
+            double velocidad, tiempo;
+            if (!double.TryParse(txtVelocidad.Text, out velocidad) || !double.TryParse(txtTiempo.Text, out tiempo))
+            {
+                MessageBox.Show("Por favor, ingrese valores numéricos válidos.", "Datos inválidos",
+                              MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Validar que los valores sean positivos
+            if (velocidad < 0 || tiempo < 0)
+            {
+                MessageBox.Show("Por favor, ingrese valores positivos.", "Datos inválidos",
+                              MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Calcular la distancia: distancia = velocidad × tiempo
+            double distancia = velocidad * tiempo;
+
+            // Mostrar el resultado en el textBox de resultado
+            txtRespuesta.Text = distancia.ToString("F2") + " unidades";
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            // Limpiar todos los textBox
+            txtVelocidad.Text = "";
+            txtTiempo.Text = "";
+            txtRespuesta.Text = "";
+
+            // Colocar el foco en el primer textBox
+            txtVelocidad.Focus();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            // Salir de la aplicación
+            Application.Exit();
+        }
+    }
+}
